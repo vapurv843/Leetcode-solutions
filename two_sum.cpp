6f9f10e56ec42530b64473d2c72966c94424c611
Apurv verma <41693158+vapurv@users.noreply.github.com>
2020-07-22 07:22:56 -0700

    Add files via upload

---
+++ two_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> nums, int target) {
+        vector<int>tmp;
+        for(int i = 0; i < nums.size()-1; i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target){
+                    tmp.push_back(i);
+                    tmp.push_back(j);
+                    return tmp;
+                }
+            }
+        }
+       return tmp; 
+    }
+};
