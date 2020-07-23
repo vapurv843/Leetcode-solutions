b3f6a369adbd932ebeae6cf9b473d2156b7a7433
Apurv verma <41693158+vapurv@users.noreply.github.com>
2020-07-23 10:56:10 -0700

    Add files via upload

---
+++ palindrome_number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long int ne=0;
+    bool isPalindrome(int x) {
+        int k = x;
+        if(x<0)
+        {
+            return false;
+        }
+            
+        else
+        {
+            while(x!=0)
+            {
+            int k = x%10;
+            ne = ne*10+k;
+            x = x/10;
+            }
+            if(ne==k)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+};
