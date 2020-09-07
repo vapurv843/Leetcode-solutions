7c9c4ad7d204899f6db8a4b2628a86740cb6096a
Apurv verma <41693158+vapurv@users.noreply.github.com>
2020-09-07 11:22:21 -0700

    Update reverse_integer.cpp

--- reverse_integer.cpp
+++ reverse_integer.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    long long int ne=0;
+    long long int net=0;
     int reverse(int x) {
         
         while(x!=0)
         {
             int k = x%10;
-            ne = ne*10+k;
+            net = net*10+k;
             x=x/10;
         }
-       if(ne>=INT_MIN&&ne<=INT_MAX){return ne;}
-return 0;
+       if(net>=INT_MIN&&ne<=INT_MAX)
+       {
+           return net;
+       }
+        return 0;
         
     }
 };
