a29ff0471266919d8581642556b94acd76cb10bf
Apurv verma <41693158+vapurv@users.noreply.github.com>
2020-09-16 21:44:42 -0700

    update 1

--- maximum_sum_triplet.cpp
+++ maximum_sum_triplet.cpp
@@ -5,8 +5,7 @@ int Solution::solve(vector<int> &arr) {
     for (int i = 1; i < n - 1; ++i) { 
         int max1 = 0, max2 = 0; 
   
-        // find maximum value(less than arr[i]) 
-        // from i+1 to n-1 
+        
         for (int j = 0; j < i; ++j) 
             if (arr[j] < arr[i]) 
                 max1 = max(max1, arr[j]); 
