bf8564c38bf333507ac5153e2c17c68a2d0f6508
Apurv verma <41693158+vapurv@users.noreply.github.com>
2020-09-24 01:08:17 -0700

    change 1

--- merge_arrays.cpp
+++ merge_arrays.cpp
@@ -25,16 +25,7 @@ void merge(int X[], int Y[], int m, int n)
 		if (X[i] > Y[0])
 		{
 			swap(X[i], Y[0]);
-			int first = Y[0];
-
-			// move Y[0] to its correct position to maintain sorted
-			// order of Y[]. Note: Y[1..n-1] is already sorted
-			int k;
-			for (k = 1; k < n && Y[k] < first; k++) {
-				Y[k - 1] = Y[k];
-			}
-
-			Y[k - 1] = first;
+			sort(Y,Y+n);
 		}
 	}
 }
