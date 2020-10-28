af598787806b316e61ae1d7e8153bff925a9d450
Apurv verma <vapurv843@gmail.com>
2020-10-28 12:11:43 -0700

    gfg

---
+++ search_in_rotated_array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+    while (l<=r) {
+        int mid = (r-l)/2+l;
+        if (nums[mid] == target)
+            return mid;
+        if (nums[mid] < nums[r]) {
+            if (nums[mid]<target && target<=nums[r])
+                l = mid+1;
+            else
+                r = mid-1;
+        } else {
+            if(nums[l]<=target && target<nums[mid])
+                r = mid-1;
+            else
+                l = mid+1;
+        }
+    }
+    return -1;
+        
+    }
+};
