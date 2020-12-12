87fc1e67145fe9897e3da5886ab612d3300cef16
Apurv verma <vapurv843@gmail.com>
2020-12-12 10:37:55 -0800

    gfg

---
+++ josephus_problem.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std; 
+  
+int josephus(int n, int k) 
+{ 
+    if (n == 1) 
+        return 1; 
+    else
+        
+        return (josephus(n - 1, k) + k-1) % n + 1; 
+} 
+  
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        
+        cout << josephus(n, k)<<endl;
+    //return 0; 
+    }
+} 
