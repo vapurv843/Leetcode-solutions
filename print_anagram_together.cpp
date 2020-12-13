47a1fb9f0503b5ed9ee8842d30b92b87adccdc2a
Apurv verma <vapurv843@gmail.com>
2020-12-13 02:53:40 -0800

    gfg_hash

---
+++ print_anagram_together.cpp
@@ -0,0 +1,30 @@
+vector<vector<string> > Anagrams(vector<string>& strings) 
+{   
+    unordered_map<string,int> umap;
+     unordered_map<string,vector<string>> ans;
+    for(int i=0;i<strings.size();i++)
+    {
+        string s=strings[i];
+        sort(s.begin(),s.end());
+        if(umap.find(s)!=umap.end())
+        {
+            //umap[s]++;
+            ans[s].push_back(strings[i]);
+        }
+        else
+        {
+            umap[s]++;
+            ans[s].push_back(strings[i]);
+        }
+    }
+    
+    
+    vector<vector<string>> ans1;
+    for(auto x:ans)
+    {
+        ans1.push_back(x.second);
+    }
+    return ans1;
+
+
+}
