fe0cb6ed4f1f07cf35fb60a83af98ab341323ca2
Apurv verma <vapurv843@gmail.com>
2020-12-16 02:42:13 -0800

    gfg

---
+++ count_zero_in_sorted_array.cpp
@@ -0,0 +1,23 @@
+class Solution{   
+public:
+    int countZeroes(int arr[], int n) {
+        if(arr[n-1]==1) return 0;
+        if(arr[0]==0) return n;
+        int l= 0;
+        int h = n-1;
+        while(l<h)
+        {
+            int mid = (l+h)/2;
+            if(arr[mid ]==0)
+            {
+                h = mid;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+
+            return n-h;
+    }
+};
