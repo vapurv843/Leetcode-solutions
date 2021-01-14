903071acedb368b402563a68785a8572bf1c475a
Apurv verma <vapurv843@gmail.com>
2021-01-13 22:05:01 -0800

    gfg

---
+++ largest_number_with_given_sum.cpp
@@ -0,0 +1,28 @@
+string largestNumber(int n, int sum){
+    // Your code here
+    string output(n, '0');
+    
+    for(int i = 0; i < n; i++)
+    {
+        int val = 0;
+        if(sum > 9)
+        {
+            val = 9;
+            sum -= 9;
+        }
+        else
+        {
+            val = sum;
+            sum = 0;
+        }
+        
+        output[i] = val + '0';
+    }
+    
+    if(sum > 0)
+    {
+        return "-1";
+    }
+    
+    return output;
+}
