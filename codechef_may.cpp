98fcbdf793804bcc610b0e852f3ef49b3e6ace2e
Apurv verma <vapurv843@gmail.com>
2021-05-31 20:59:12 +0530

    may lunchtime

---
+++ codechef_may.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n ;
+    cin>>n;
+    int start = 1;
+    int end = n;
+    int ans = 0;
+   ans = (start+end)/2;
+    cout<<ans<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
