b3a640976a722ae17d252883c61d7382e34cbbae
Apurv verma <vapurv843@gmail.com>
2021-06-10 13:54:06 +0530

    leetcode

---
+++ inorder_bst_iterative.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> s;
+        vector<int> v;
+        TreeNode *curr = root;
+        while(curr!=NULL or s.empty()==false)
+        {
+            while(curr!=NULL)
+            {
+                s.push(curr);
+                curr = curr->left;
+            }
+            curr = s.top();
+            s.pop();
+            v.push_back(curr->val);
+            curr = curr->right;
+            
+        }
+        return v;
+        
+    }
+};
