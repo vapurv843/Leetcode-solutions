ce6375076119202c924a0b808adb48bc71376877
Apurv verma <vapurv843@gmail.com>
2021-06-11 18:06:52 +0530

    leetcode

---
+++ divisor_game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n%2)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+};
