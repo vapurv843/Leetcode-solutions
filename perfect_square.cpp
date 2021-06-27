98ec035e124fa147db0d4835bedfc003454f187c
Apurv verma <vapurv843@gmail.com>
2021-06-27 11:00:34 +0530

    binary search

---
+++ perfect_square.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int start =1;
+        int end = num;
+        while(start<=end)
+        {
+            long long mid = start+(end-start)/2;
+            if(mid*mid==num)
+            {
+                return true;
+            }
+            if(mid*mid<num)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+};
