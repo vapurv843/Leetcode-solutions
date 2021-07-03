97986e7f4fcf9afb9b43a7be4348a0a38dccb459
Apurv verma <vapurv843@gmail.com>
2021-07-03 20:23:07 +0530

    another method added

--- peak_element.cpp
+++ peak_element.cpp
@@ -13,3 +13,27 @@ public:
         
     }
 };
+
+
+
+//Another solution 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int res = 0;
+        int n = nums.size();
+        if(nums.size()==1) return 0;
+        if(nums[0]>=nums[1]) return 0;
+        if (nums[n - 1] >= nums[n - 2])
+            return n - 1;
+        for(int i = 1;i<nums.size()-1;i++)
+        {
+            if(nums[i]>nums[i+1] and nums[i]>nums[i-1])
+            {
+                return i;
+            }
+        }
+        return nums.size()-1;
+        
+    }
+};
