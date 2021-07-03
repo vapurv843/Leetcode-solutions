096bef087ad72a42a7a494b0af7beabef8509ed3
Apurv verma <vapurv843@gmail.com>
2021-07-03 15:45:04 +0530

    leetcode

---
+++ peak_element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0;i<nums.size()-1;i++)
+        {
+            if(nums[i]>nums[i+1])
+            {
+                return i;
+            }
+        }
+        return nums.size()-1;
+        
+    }
+};
