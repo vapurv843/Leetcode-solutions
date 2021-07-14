abdac563dfd6cec14fb49fb559f64c6b3c818fde
Apurv verma <vapurv843@gmail.com>
2021-07-14 21:10:00 +0530

    leetcode

---
+++ codeforces_div2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std ;
+const int n =50001;
+typedef long long ll;
+vector<int> v(n);
+void solve()
+{
+    ll s;
+    cin>>s;
+
+    
+    cout<<ceil(sqrt(s))<<endl;
+}
+ 
+int main()
+{
+    
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+}
