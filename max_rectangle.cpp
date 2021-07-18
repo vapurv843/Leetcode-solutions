6a00941d542ab40baba91e0dcb810d42806ce575
Apurv verma <vapurv843@gmail.com>
2021-07-18 20:12:08 +0530

    leetcode

---
+++ max_rectangle.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        for(int mid = sqrt(area); mid>0; mid--)
+            if (!(area%mid))
+                return {area/mid, mid};
+        return{};
+        
+    }
+};
