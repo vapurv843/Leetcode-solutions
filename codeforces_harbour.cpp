13d3e23ab755711b1bd2e6ef2da699310c08bb78
Apurv verma <vapurv843@gmail.com>
2021-07-22 21:57:18 +0530

    leetcode

---
+++ codeforces_harbour.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    long long s;
+    cin>>s;
+    cout<<(s+1)/10<<endl;
+
+
+}
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
