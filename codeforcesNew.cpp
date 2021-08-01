8be9b82d0171e0a53a8087ab4e0b542c563c38ff
Apurv verma <vapurv843@gmail.com>
2021-08-01 20:53:40 +0530

    codeforces div 2

---
+++ codeforcesNew.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve()
+{
+    ll a;
+    cin>>a;
+    cout<<2<<" "<<a-1<<endl;
+}
+int main()
+{
+    ll t;
+    while(t--)
+    {
+        solve();
+    }
+}
