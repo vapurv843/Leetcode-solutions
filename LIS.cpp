cb03affe511be980daff036fbd6947226edac240
Apurv verma <vapurv843@gmail.com>
2021-09-28 23:30:35 +0530

    leetcode

---
+++ LIS.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> lis(nums.size(),1);
+       
+        int l = 1;
+        for(int i = 1;i<nums.size();i++)
+        {
+            
+            for(int j = 0;j<i;j++)
+            {
+                if(nums[i]>nums[j] )
+                {
+                    lis[i] = max(lis[i],lis[j]+1);
+                }
+            }
+        }
+        return *max_element(lis.begin(),lis.end());
+        
+        
+    }
+};
